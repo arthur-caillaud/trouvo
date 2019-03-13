82b74d897bc07a5151dc1e989fcfa29dfdd5cd4f
Arthur Caillaud <arthurcaillaud@gmail.com>
2019-03-13 19:42:02 +0100

    Trying a first version for persist package

--- main.go
+++ main.go
@@ -10,79 +10,30 @@ import (
 	"trouvo/display"
 	"trouvo/indexer"
 	"trouvo/parser"
+	"trouvo/persist"
 	"trouvo/search"
 )
 
 const (
-	pathNameCACM      = "/Users/arthur/go/src/trouvo/Data/CACM/cacm.all"
-	pathNameCS276     = "/Users/arthur/go/src/trouvo/Data/CS276"
-	stopWordsPathName = "/Users/arthur/go/src/trouvo/Data/CACM/common_words"
+	pathNameCACM       = "/Users/arthur/go/src/trouvo/Data/CACM/cacm.all"
+	indexPathNameCACM  = "/Users/arthur/go/src/trouvo/Data/CACM/index.idx"
+	pathNameCS276      = "/Users/arthur/go/src/trouvo/Data/CS276"
+	indexPathNameCS276 = "/Users/arthur/go/src/trouvo/Data/CS276/index.idx"
+	stopWordsPathName  = "/Users/arthur/go/src/trouvo/Data/CACM/common_words"
 )
 
 func main() {
-	mainCS276()
+	mainCACM()
 }
 
 func mainCACM() {
-
-	start := time.Now()
-	p := parser.New(pathNameCACM, stopWordsPathName)
-	p.Run() // Parsing...
-	col := p.GetCollection()
-	end := time.Now()
-	elapsed := end.Sub(start)
-	fmt.Println("Parsed in", elapsed)
-	fmt.Println("----")
-
-	start = time.Now()
-	docs := col.GetDocs()
-	stopWords := p.GetStopWords()
-	for _, doc := range docs {
-		doc.Tokenize()
-		doc.FilterTokens(stopWords)
-	}
-	end = time.Now()
-	elapsed = end.Sub(start)
-	fmt.Println("Tokenized in", elapsed)
-	fmt.Println("----")
-
-	start = time.Now()
-	col.BuildVocabulary()
-	fmt.Println(len(col.GetVocabulary()), "words in vocabulary")
-	end = time.Now()
-	elapsed = end.Sub(start)
-	fmt.Println("Vocabulary built in", elapsed)
-	fmt.Println("----")
-
-	start = time.Now()
-	indexer := indexer.New(col)
-	indexer.Build()
-	end = time.Now()
-	elapsed = end.Sub(start)
-	fmt.Println("Indexed in", elapsed)
-	fmt.Println("Index is", indexer.GetIndexSize(), "kB large.")
-	fmt.Println("----")
-
-	engine := search.NewSearchEngine(
-		indexer.GetIndex(),
-		indexer.GetVocDict(),
-		indexer.GetIdfDict(),
-		indexer.GetDocDict(),
-		indexer.GetDocNormDict(),
-	)
-	disp := display.New(indexer.GetDocDict())
-
-	// Main infinite loop used to let the user query our search engine
-	for true {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Println("Type query :")
-		text, _ := reader.ReadString('\n')
-		start := time.Now()
-		text = strings.TrimSpace(text)
-		res := engine.VectSearch(text)
-		end := time.Now()
-		elapsed := end.Sub(start)
-		disp.Show(res, elapsed)
+	// load the indexer if stored on disk
+	var indexer indexer.Indexer
+	if err := persist.Load(indexPathNameCACM, indexer); err != nil {
+		fmt.Println(err)
+		buildCACM()
+	} else {
+		runCACM(indexer)
 	}
 }
 
@@ -144,6 +95,74 @@ func mainCS276() {
 		start := time.Now()
 		text = strings.TrimSpace(text)
 		res := superEngine.Search(text)
+		end := time.Now()
+		elapsed := end.Sub(start)
+		disp.Show(res, elapsed)
+	}
+}
+
+func buildCACM() {
+	start := time.Now()
+	p := parser.New(pathNameCACM, stopWordsPathName)
+	p.Run() // Parsing...
+	col := p.GetCollection()
+	end := time.Now()
+	elapsed := end.Sub(start)
+	fmt.Println("Parsed in", elapsed)
+	fmt.Println("----")
+
+	start = time.Now()
+	docs := col.GetDocs()
+	stopWords := p.GetStopWords()
+	for _, doc := range docs {
+		doc.Tokenize()
+		doc.FilterTokens(stopWords)
+	}
+	end = time.Now()
+	elapsed = end.Sub(start)
+	fmt.Println("Tokenized in", elapsed)
+	fmt.Println("----")
+
+	start = time.Now()
+	col.BuildVocabulary()
+	fmt.Println(len(col.GetVocabulary()), "words in vocabulary")
+	end = time.Now()
+	elapsed = end.Sub(start)
+	fmt.Println("Vocabulary built in", elapsed)
+	fmt.Println("----")
+
+	start = time.Now()
+	indexer := indexer.New(col)
+	indexer.Build()
+	end = time.Now()
+	elapsed = end.Sub(start)
+	fmt.Println("Indexed in", elapsed)
+	fmt.Println("Index is", indexer.GetIndexSize(), "kB large.")
+	fmt.Println("----")
+
+	if err := persist.Save(indexPathNameCACM, &indexer); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func runCACM(indexer indexer.Indexer) {
+	engine := search.NewSearchEngine(
+		indexer.GetIndex(),
+		indexer.GetVocDict(),
+		indexer.GetIdfDict(),
+		indexer.GetDocDict(),
+		indexer.GetDocNormDict(),
+	)
+	disp := display.New(indexer.GetDocDict())
+
+	// Main infinite loop used to let the user query our search engine
+	for true {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("Type query :")
+		text, _ := reader.ReadString('\n')
+		start := time.Now()
+		text = strings.TrimSpace(text)
+		res := engine.VectSearch(text)
 		end := time.Now()
 		elapsed := end.Sub(start)
 		disp.Show(res, elapsed)
