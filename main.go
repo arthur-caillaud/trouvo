1f83d3f1410782c5bbf70b51fd561f01df3d6c91
Arthur Caillaud <arthurcaillaud@gmail.com>
2019-03-14 09:36:53 +0100

    Rounding performance measures

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ const (
 )
 
 func main() {
-	mainCACM()
+	mainCS276()
 }
 
 func mainCACM() {
@@ -36,13 +36,79 @@ func mainCS276() {
 	runCS276(indexers)
 }
 
+func buildCACM() *indexer.Indexer {
+	start := time.Now()
+	p := parser.New(pathNameCACM, stopWordsPathName)
+	p.Run() // Parsing...
+	col := p.GetCollection()
+	end := time.Now()
+	elapsed := end.Sub(start)
+	fmt.Println("Parsed in", elapsed.Round(time.Millisecond))
+	fmt.Println("----")
+
+	start = time.Now()
+	docs := col.GetDocs()
+	stopWords := p.GetStopWords()
+	for _, doc := range docs {
+		doc.Tokenize()
+		doc.FilterTokens(stopWords)
+	}
+	end = time.Now()
+	elapsed = end.Sub(start)
+	fmt.Println("Tokenized in", elapsed.Round(time.Millisecond))
+	fmt.Println("----")
+
+	start = time.Now()
+	col.BuildVocabulary()
+	fmt.Println(len(col.GetVocabulary()), "words in vocabulary")
+	end = time.Now()
+	elapsed = end.Sub(start)
+	fmt.Println("Vocabulary built in", elapsed.Round(time.Millisecond))
+	fmt.Println("----")
+
+	start = time.Now()
+	indexer := indexer.New(col)
+	indexer.Build()
+	end = time.Now()
+	elapsed = end.Sub(start)
+	fmt.Println("Indexed in", elapsed.Round(time.Millisecond))
+	fmt.Println("Index is", indexer.GetIndexSize(), "kB large.")
+	fmt.Println("----")
+
+	return indexer
+}
+
+func runCACM(indexer *indexer.Indexer) {
+	engine := search.NewSearchEngine(
+		indexer.GetIndex(),
+		indexer.GetVocDict(),
+		indexer.GetIdfDict(),
+		indexer.GetDocDict(),
+		indexer.GetDocNormDict(),
+	)
+	disp := display.New(indexer.GetDocDict())
+
+	// Main infinite loop used to let the user query our search engine
+	for true {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("Type query :")
+		text, _ := reader.ReadString('\n')
+		start := time.Now()
+		text = strings.TrimSpace(text)
+		res := engine.VectSearch(text)
+		end := time.Now()
+		elapsed := end.Sub(start).Round(time.Microsecond)
+		disp.Show(res, elapsed)
+	}
+}
+
 func buildCS276() []*indexer.Indexer {
 	start := time.Now()
 	p := cs276parser.New(pathNameCS276)
 	p.Run() // Parsing...
 	end := time.Now()
 	elapsed := end.Sub(start)
-	fmt.Println("Parsed in", elapsed)
+	fmt.Println("Parsed in", elapsed.Round(time.Second))
 	fmt.Println("----")
 
 	start = time.Now()
@@ -52,7 +118,7 @@ func buildCS276() []*indexer.Indexer {
 	}
 	end = time.Now()
 	elapsed = end.Sub(start)
-	fmt.Println("Vocabulary built in", elapsed)
+	fmt.Println("Vocabulary built in", elapsed.Round(time.Millisecond))
 	fmt.Println("----")
 
 	start = time.Now()
@@ -66,7 +132,7 @@ func buildCS276() []*indexer.Indexer {
 	}
 	end = time.Now()
 	elapsed = end.Sub(start)
-	fmt.Println("Indexed in", elapsed)
+	fmt.Println("Indexed in", elapsed.Round(time.Second))
 	fmt.Println("Index is", indexSize, "kB large.")
 	fmt.Println("----")
 	return indexers
@@ -107,74 +173,8 @@ func runCS276(indexers []*indexer.Indexer) {
 		// Run the query
 		res := superEngine.Search(text)
 		end := time.Now()
-		elapsed := end.Sub(start)
+		elapsed := end.Sub(start).Round(time.Millisecond)
 		// Display the results
-		disp.Show(res, elapsed)
-	}
-}
-
-func buildCACM() *indexer.Indexer {
-	start := time.Now()
-	p := parser.New(pathNameCACM, stopWordsPathName)
-	p.Run() // Parsing...
-	col := p.GetCollection()
-	end := time.Now()
-	elapsed := end.Sub(start)
-	fmt.Println("Parsed in", elapsed)
-	fmt.Println("----")
-
-	start = time.Now()
-	docs := col.GetDocs()
-	stopWords := p.GetStopWords()
-	for _, doc := range docs {
-		doc.Tokenize()
-		doc.FilterTokens(stopWords)
-	}
-	end = time.Now()
-	elapsed = end.Sub(start)
-	fmt.Println("Tokenized in", elapsed)
-	fmt.Println("----")
-
-	start = time.Now()
-	col.BuildVocabulary()
-	fmt.Println(len(col.GetVocabulary()), "words in vocabulary")
-	end = time.Now()
-	elapsed = end.Sub(start)
-	fmt.Println("Vocabulary built in", elapsed)
-	fmt.Println("----")
-
-	start = time.Now()
-	indexer := indexer.New(col)
-	indexer.Build()
-	end = time.Now()
-	elapsed = end.Sub(start)
-	fmt.Println("Indexed in", elapsed)
-	fmt.Println("Index is", indexer.GetIndexSize(), "kB large.")
-	fmt.Println("----")
-
-	return indexer
-}
-
-func runCACM(indexer *indexer.Indexer) {
-	engine := search.NewSearchEngine(
-		indexer.GetIndex(),
-		indexer.GetVocDict(),
-		indexer.GetIdfDict(),
-		indexer.GetDocDict(),
-		indexer.GetDocNormDict(),
-	)
-	disp := display.New(indexer.GetDocDict())
-
-	// Main infinite loop used to let the user query our search engine
-	for true {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Println("Type query :")
-		text, _ := reader.ReadString('\n')
-		start := time.Now()
-		text = strings.TrimSpace(text)
-		res := engine.VectSearch(text)
-		end := time.Now()
-		elapsed := end.Sub(start)
 		disp.Show(res, elapsed)
 	}
 }
